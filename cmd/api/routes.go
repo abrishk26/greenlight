3bf9ac9b7d1de4eb5579053bcb474533ab38312a
Abreham <abrehamkassa19@gmail.com>
2025-02-14 00:50:06 +0300

    Define a function that returns a router with registered handlers

---
+++ cmd/api/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (a *application) routes() *httprouter.Router {
+	
+	router := httprouter.New()
+	
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", a.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", a.showMovieHandler)
+	
+	return router
+}
