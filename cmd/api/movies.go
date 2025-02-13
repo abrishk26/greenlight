571fb6ed1c6862944bb622b9c9ac2bf4081bcda8
Abreham <abrehamkassa19@gmail.com>
2025-02-14 01:00:03 +0300

    Add helper function to extract id parameter from request

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func (a *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,15 +10,13 @@ func (a *application) createMovieHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (a *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-	
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	
+
+	id, err := a.readIDParam(r)
+
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-	
+
 	fmt.Fprintf(w, "Showing details of movies with id: %d", id)
 }
-
