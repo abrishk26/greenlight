ce61e70ba0d71bee6e7b2175b9ff1a1a80a8d408
Abreham <abrehamkassa19@gmail.com>
2025-02-14 13:10:21 +0300

    Add a function that return a json response to a request

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -18,4 +19,24 @@ func(a *application) readIDParam(r *http.Request) (int64, error) {
 	}
 	
 	return id, nil
+}
+
+func (a *application) writeJSON(w http.ResponseWriter, data interface{}, headers http.Header, status int) error {
+	js, err := json.MarshalIndent(data, "", "  ")
+	
+	if err != nil {
+		return err
+	}
+	
+	js = append(js, '\n')
+	
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	
+	return nil
 }
