5af9b3599fe5a9879f0aab73042d830541e38032
Abreham <abrehamkassa19@gmail.com>
2025-02-18 19:27:07 +0300

    Update the app instance to include database models

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,19 +1,66 @@
 package data
 
 import (
+	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/abrishk26/greenlight/internal/validator"
+	"github.com/lib/pq"
 )
 
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+		INSERT INTO movies(title, runtime, year, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version
+	`
+
+	args := []interface{}{movie.Title, movie.Runtime, movie.Year, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	movie := &Movie{}
+	query := `
+		SELECT * FROM movies
+		WHERE id = $1
+	`
+
+	err := m.DB.QueryRow(query, id).Scan(&movie.ID, &movie.CreatedAt, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return movie, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int32) error {
+	return nil
+}
+
 type Movie struct {
-	ID int64 `json:"id"`
+	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
-	Title string `json:"title"`
-	Year int32 `json:"year,omitempty"`
-	Runtime int32 `json:"runtime,omitempty"`
-	Genres []string `json:"genres,omitempty"`
-	Version int32 `json:"version"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
 }
 
 func ValidateMovie(v *validator.Validator, m *Movie) {
