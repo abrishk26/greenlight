358969aad7cd981a8b0e4c599e1f0f8bc8d15f8f
Abreham <abrehamkassa19@gmail.com>
2025-02-22 17:10:51 +0300

    Add graceful shutdown to the server

---
+++ cmd/api/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (a *application) serve() error {
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", a.conf.port),
+		Handler: a.routes(),
+		// ErrorLog: log.New(logger, "", 0),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	shutDownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		a.logger.PrintInfo("shutting down server", map[string]string{
+			"signal": s.String(),
+		})
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutDownError <- srv.Shutdown(ctx)
+	}()
+
+	a.logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  a.conf.env,
+	})
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutDownError
+
+	if err != nil {
+		return err
+	}
+
+	a.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+	return nil
+}
