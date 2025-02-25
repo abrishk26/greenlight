75c33c37d40adf1402022f172be0fc0fdfed5738
Abreham <abrehamkassa19@gmail.com>
2025-02-25 19:07:42 +0300

    Modify it to send an activation token for the user during registration via email

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/abrishk26/greenlight/internal/data"
 	"github.com/abrishk26/greenlight/internal/validator"
@@ -52,8 +53,20 @@ func (a *application) registerUserHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	token, err := a.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		a.serverErrorResponse(w, r, err)
+		return
+	}
+
 	fn := func() {
-		err = a.mailer.Send(user.Email, "user_welcome.tmpl", user)
+
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID": user.ID,
+		}
+
+		err = a.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			a.logger.PrintError(err, nil)
 			return
